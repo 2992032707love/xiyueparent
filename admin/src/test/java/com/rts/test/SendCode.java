fb63d0bcf1f9c5f954e9107903bdd522ed60ff5d
Xiyue <rents@foxmail.com>
2023-08-25 21:18:04 +0800

    master commit one

--- admin/src/test/java/com/rts/test/SendCode.java
+++ admin/src/test/java/com/rts/test/SendCode.java
@@ -103,7 +103,7 @@ public class SendCode {
         System.out.println(code.getClass().getName());
         System.out.println(OLDCODE.getClass().getName());
         String sendCode = getSendCode();
-        System.out.println("验证码为：" + sendCode);
+        System.out.println("验证码为：" + sendCode + "YZM为：" + YZM);
     }
     public static String getSendCode() {
         log.info("验证码生成！！！！");
