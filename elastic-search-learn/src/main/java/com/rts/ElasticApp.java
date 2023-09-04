002986b35553281665d139a4a9ccd05ce0c0c41c
Xiyue <rents@foxmail.com>
2023-09-04 21:28:53 +0800

    Elastic-search-learn commit test

---
+++ elastic-search-learn/src/main/java/com/rts/ElasticApp.java
@@ -0,0 +1,11 @@
+package com.rts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticApp.class);
+    }
+}
